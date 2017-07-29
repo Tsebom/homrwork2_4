e40d97ee8c150fbdc8a96fd2fc25073aa3fd58fb
Nezhdanov Sergey <Tsebom@yandex.ru>
2017-07-29 14:24:58 +0400

    Работа с относительными путями

---
+++ homework2_4.py
@@ -0,0 +1,42 @@
+import os
+
+def file_sql(list_file):
+	"""Возвращает список фаилов с расширением .sql, аргумент - (исходный список фаилов)"""
+	file_list_sql = []
+	for name in list_file:
+		if name[len(name) - 4 :] == '.sql':
+			file_list_sql.append(name)
+	return file_list_sql
+
+def input_str():
+	"""Запрашивет искомую строку"""
+	search_string = input('Введите строку: ')
+	return search_string.strip()
+
+def open_file(file, search, directory):
+	"""Открывает фаил и проверяет вхождение искомой строки, аргументы - (имя фаила, искомая строка, директория с фаилами)""" 
+	with open(os.path.join(directory, file)) as f:
+		data = f.read()
+		if data.count(search) == 0:
+			return False
+		return True
+
+def list_search_file(list, directory):
+	"""Возвращает список фаилов удовлетворивших поискову запрос, аргумент - (исходный список, директория с фаилами)"""
+	list_file = []
+	search_str = input_str()
+	for name in list:
+		if open_file(name, search_str, directory) == True:
+			list_file.append(name)
+	print('\n'.join(list_file))
+	print('Всего: {}'.format(len(list_file)))
+	return list_file
+
+def main():
+	directory_file = os.path.join(os.path.dirname(__file__), 'Migrations')
+	list_file = os.listdir(directory_file)
+	list_file_sql = file_sql(list_file)# Список фаилов с расширением *.sql
+	while len(list_file_sql) > 1:
+		list_file_sql = list_search_file(list_file_sql, directory_file)
+
+main()
