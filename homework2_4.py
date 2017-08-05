51688780330e2cd75493f52ed24f5c696bbd3c07
Nezhdanov Sergey <Tsebom@yandex.ru>
2017-08-06 01:51:02 +0400

    Работа с замечаниями

--- homework2_4.py
+++ homework2_4.py
@@ -1,10 +1,11 @@
+import operator
 import os
 
 def file_sql(list_file):
 	"""Возвращает список фаилов с расширением .sql, аргумент - (исходный список фаилов)"""
 	file_list_sql = []
 	for name in list_file:
-		if name[len(name) - 4 :] == '.sql':
+		if operator.__eq__(os.path.splitext(name)[1], '.sql'):
 			file_list_sql.append(name)
 	return file_list_sql
 
@@ -17,16 +18,16 @@ def open_file(file, search, directory):
 	"""Открывает фаил и проверяет вхождение искомой строки, аргументы - (имя фаила, искомая строка, директория с фаилами)""" 
 	with open(os.path.join(directory, file)) as f:
 		data = f.read()
-		if data.count(search) == 0:
+		if not search in data:
 			return False
 		return True
 
-def list_search_file(list, directory):
+def list_search_file(list_, directory):
 	"""Возвращает список фаилов удовлетворивших поискову запрос, аргумент - (исходный список, директория с фаилами)"""
 	list_file = []
 	search_str = input_str()
-	for name in list:
-		if open_file(name, search_str, directory) == True:
+	for name in list_:
+		if open_file(name, search_str, directory):
 			list_file.append(name)
 	print('\n'.join(list_file))
 	print('Всего: {}'.format(len(list_file)))
@@ -36,7 +37,7 @@ def main():
 	directory_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'Migrations')
 	list_file = os.listdir(directory_file)
 	list_file_sql = file_sql(list_file)# Список фаилов с расширением *.sql
-	while len(list_file_sql) > 1:
+	while operator.__gt__(len(list_file_sql), 1):
 		list_file_sql = list_search_file(list_file_sql, directory_file)
 
 main()
